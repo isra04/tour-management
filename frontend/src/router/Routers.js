6a99902b59c0ecc48ebe882d8f90cc61fc491c74
isra04 <100473078+isra04@users.noreply.github.com>
2023-07-24 12:06:41 +0600

    Add: Project required files and libraries
    Setup Front- end and Backend project.

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './../pages/Home';
+import Tours from './../pages/Tours';
+import TourDetails from './../pages/TourDetails';
+import Login from './../pages/Login';
+import Register from './../pages/Register';
+import SearchResultList from './../pages/SearchResultList';
+import About from '../pages/About';
+
+const Routers = () => {
+    return (
+        <Routes>
+            <Route
+                path='/home'
+                element={<Home />}
+            />
+            <Route
+                path='/about'
+                element={<About />}
+            />
+            <Route
+                path='/tours'
+                element={<Tours />}
+            />
+            <Route
+                path='/tours/:id'
+                element={<TourDetails />}
+            />
+            <Route
+                path='/login'
+                element={<Login />}
+            />
+            <Route
+                path='/register'
+                element={<Register />}
+            />
+            <Route
+                path='/tours/search'
+                element={<SearchResultList />}
+            />
+        </Routes>
+    );
+};
+
+export default Routers;
---
+++ frontend/src/shared/Subtitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+const Subtitle = ({subtitle}) => {
+      return (
+        <h3 className='section_subtite'>{subtitle}</h3>
+      )
+}
+export default Subtitle;
+
